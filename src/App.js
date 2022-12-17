3593cf1af82fde46171938d0f107d470dc7a085b
emmanuelguty <manuel15901@gmail.com>
2022-12-17 13:05:41 -0600

    Se agregaron los estilos principales

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import gartLogo from './images/gartLogo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='gart-logo-contenedor'>
+        <img 
+        src={gartLogo} 
+        alt="gart logo"
+        className='gart-logo' />
+      </div>
     </div>
   );
 }
