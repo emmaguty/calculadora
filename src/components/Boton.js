c3ed02f7fd3e49c5c4a547c3da33b5f7135c26d9
emmanuelguty <manuel15901@gmail.com>
2022-12-17 16:27:27 -0600

    Se completo la funcion para saber si es operador

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -1,12 +1,13 @@
 function Boton (props) {
 
+    // Funcion para saber si es operador
     const esOperador = valor => {
         return isNaN(valor) && (valor != '.') && (valor != '=');
     }; 
 
     return (
         <div 
-        className={`boton-contenedor ${esOperador(props.children) ? 'Operador' : null}`} >
+        className={`boton-contenedor ${esOperador(props.children) ? 'Operador' : ''}`.trimEnd()} >
             {props.children}
         </div>
     )
